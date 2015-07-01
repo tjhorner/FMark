a8b2e3acd23ed7bc7b041996699f4cdafab15057
TJ Horner <me@tjhorner.com>
2015-06-30 22:56:02 -0700

    Create fmark.js

---
+++ fmark.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         FMark
+// @namespace    https://horner.tj/
+// @version      0.2
+// @description  Enables Markdown on Facebook
+// @author       TJ Horner
+// @match        *://*.facebook.com/*
+// @require      https://raw.githubusercontent.com/chjj/marked/master/lib/marked.js
+// @grant        none
+// ==/UserScript==
+
+FacebookMarkdown = (function(){
+  var getUserContent = function(){
+    return $(".userContent:not([fmark-replaced]) p, .UFICommentBody:not([fmark-replaced]) span");
+  };
+  
+  var replaceUserContent = function(){
+    var userContent = getUserContent();
+    console.log("userContent", userContent);
+    $.each(userContent, function(i, e){
+      var $e = $(e);
+      var $replacement = $(marked($e.text().replace("<", "&lt;").replace(">", "&gt;")));
+      $e.parent().attr("fmark-replaced", true);
+      $e.html($replacement[0].innerHTML);
+    });
+  };
+  
+  // listen for DOM changes, then act on them
+  var observer = new MutationObserver(replaceUserContent);
+  
+  observer.observe($("#globalContainer")[0], {
+    subtree: true,
+    childList: true,
+    attribute: true,
+  });
+}());
